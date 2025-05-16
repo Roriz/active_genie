395ec750f9dce55aa5248ea1d2caa84bc40db524
Radamés Roriz <radamesroriz@gmail.com>
2025-05-16 07:51:40 -0300

    ref: improve the error message fo invalid provider

--- lib/active_genie/clients/unified_client.rb
+++ lib/active_genie/clients/unified_client.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-require_relative './openai_client'
-require_relative './anthropic_client'
-require_relative './google_client'
-require_relative './deepseek_client'
+require_relative './providers/openai_client'
+require_relative './providers/anthropic_client'
+require_relative './providers/google_client'
+require_relative './providers/deepseek_client'
+require_relative '../errors/invalid_provider_error'
 
 module ActiveGenie
   module Clients
@@ -24,13 +25,10 @@ module ActiveGenie
             client = PROVIDER_NAME_TO_CLIENT[provider_name.to_sym]
           end
 
-          raise InvalidProviderError, 'Client is not valid' if client.nil?
+          raise ActiveGenie::InvalidProviderError.new(provider_name) if client.nil?
 
           client.new(config).function_calling(messages, function)
         end
-
-        # TODO: improve error message
-        class InvalidProviderError < StandardError; end
       end
     end
   end
