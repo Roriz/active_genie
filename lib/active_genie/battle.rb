7710919293712db8c9dd802395a633dc5d201133
Radamés Roriz <radamesroriz@gmail.com>
2025-07-12 15:02:27 -0300

    BREAKING CHANGES on battle module - feat: create fight module and structure resonse for battle (#56)
    * feat: create initial fight module with very simple test
    
    * refactor: replace alias_method with explicit method definitions and simplify winner/loser assignment
    
    * chore: bump version from 0.27.2 to 0.28.0

--- lib/active_genie/battle.rb
+++ lib/active_genie/battle.rb
@@ -1,18 +1,27 @@
 # frozen_string_literal: true
 
 require_relative 'battle/generalist'
+require_relative 'battle/fight'
 
 module ActiveGenie
   # See the [Battle README](lib/active_genie/battle/README.md) for more information.
   module Battle
     module_function
 
+    def generalist(...)
+      Generalist.call(...)
+    end
+
     def call(...)
       Generalist.call(...)
     end
 
-    def generalist(...)
+    def compare(...)
       Generalist.call(...)
+    end
+
+    def fight(...)
+      Fight.call(...)
     end
   end
 end
