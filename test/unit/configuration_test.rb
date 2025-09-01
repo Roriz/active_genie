d42c15d8bd73e4894de73fcb8ccc39ba2bfbfad6
Radames Roriz <radamesroriz@gmail.com>
2025-08-31 22:29:31 -0300

    fix: set provider API key in setup for DebateTest, ConfigurationTest, and WithExplanationTest

--- test/unit/configuration_test.rb
+++ test/unit/configuration_test.rb
@@ -7,6 +7,7 @@ module ActiveGenie
   class ConfigurationTest < Minitest::Test
     def setup
       ActiveGenie.configuration.providers.all.each do |provider_name, provider|
+        provider.api_key = "#{provider_name}_secret"
         fixture_path = "#{__dir__}/extractor/fixtures/with_explanation/#{provider_name}.json"
         stub_request(:post, /#{provider.api_url}.*$/).to_return(status: 200, body: File.read(fixture_path))
       end
